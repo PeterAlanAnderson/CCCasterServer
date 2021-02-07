9e5ce2dbae2b51164405100405f54fb92e991d6d
Peter Anderson <peter.alan.anderson@gmail.com>
2021-02-07 16:52:45 -0600

    CSS and JSX changes

--- src/client/src/components/NavBar/NavBar.js
+++ src/client/src/components/NavBar/NavBar.js
@@ -6,19 +6,20 @@ import Nav from 'react-bootstrap/Nav';
 const NavBar = () => {
   return (
     <div>
-      <Navbar className='navigation-bar font-family' variant='dark' expand='lg'>
-        <Nav className='nav-brand'>
-          <Navbar.Brand href='/' className='nav-brand navbar-brand'>
-            Melty Connect
+      <Navbar className="navigation-bar" variant="dark" expand="lg">
+        <Nav className="nav-brand">
+          <Navbar.Brand href="/" className="nav-brand navbar-brand">
+            <span className="logo-melty">MELTY</span>
+            <span className="logo-connect">CONNECT</span>
           </Navbar.Brand>
         </Nav>
-        <Navbar.Toggle aria-controls='basic-navbar-nav' />
-        <Navbar.Collapse id='basic-navbar-nav' className='dropdown'>
-          <Nav className='ml-auto'>
-            <Nav.Link href='/home' className='navigation-text'>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="dropdown">
+          <Nav className="ml-auto">
+            <Nav.Link href="/home" className="navigation-text">
               Home
             </Nav.Link>
-            <Nav.Link href='/login' className='navigation-text'>
+            <Nav.Link href="/login" className="navigation-text">
               Account
             </Nav.Link>
           </Nav>
--- src/client/src/views/Landing.js
+++ src/client/src/views/Landing.js
@@ -4,49 +4,43 @@ import Button from 'react-bootstrap/Button';
 
 const Landing = () => {
   return (
-    <div className='landing-container'>
-      <div className='d-flex justify-content-center'>
-        <Button className='landing-button mt-4' variant='light'>
-          Sign Up
+    <div className="landing-container">
+      <div className="d-flex justify-content-left">
+        <Button className="landing-button" variant="light">
+          SIGN UP
         </Button>
       </div>
 
-      <p className='landing-text d-flex justify-content-center mb-2'>
-        Out of the bathroom and onto the main stage.
+      <p className="landing-text">
+        Out of the bathroom and onto the main stage.<br></br>
+        We've brought you some long overdue
+        <span className="bold"> Matchmaking </span>
+        to pair with that buttery smooth
+        <span className="bold"> Rollback Netcode</span>
       </p>
-      <p className='landing-text d-flex justify-content-center my-2'>
-        We've brought you some long overdue&nbsp;
-        <span className='bold'>Matchmaking</span>
-      </p>
-      <p className='landing-text d-flex justify-content-center my-2'>
-        to pair with that buttery smooth&nbsp;
-        <span className='bold'>Rollback Netcode</span>
-      </p>
-      <div className='d-flex justify-content-center'>
-        <Button className='landing-button mt-4' variant='light'>
-          Get the Game
+      <div className="d-flex">
+        <Button className="landing-button" variant="light">
+          <a href="https://play.meltyblood.club">GET THE GAME</a>
         </Button>
       </div>
 
-      <p className='landing-text d-flex justify-content-center mb-2'>
-        No account needed to play&nbsp;
-        <span className='bold'>Regional Matchmaking</span>.
+      <p className="landing-text">
+        No account needed to play
+        <span className="bold"> Regional Matchmaking</span>.<br></br>
+        <span className="bold">Ranked Mode </span>
+        and so much more coming soon.
       </p>
-      <p className='landing-text d-flex justify-content-center my-2'>
-        <span className='bold'>Ranked Mode&nbsp;</span> and so much more coming
-        soon.
-      </p>
-      <div className='d-flex justify-content-center'>
-        <Button className='landing-button mt-4' variant='light'>
-          Support Us
+      <div className="d-flex">
+        <Button className="landing-button" variant="light">
+          SUPPORT
         </Button>
       </div>
 
-      <p className='landing-text d-flex justify-content-center my-2'>
-        Please consider supporting us directly by donating or
-      </p>
-      <p className='landing-text d-flex justify-content-center my-2'>
-        subscribing with Ko-fi or Patreon.
+      <p className="landing-text">
+        Please consider supporting us directly by donating or subscribing with
+        <span className="bold"> Ko-fi </span>
+        or
+        <span className="bold"> Patreon</span>.
       </p>
     </div>
   );
